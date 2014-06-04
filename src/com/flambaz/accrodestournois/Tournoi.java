86f12d2e3b8901382fdef897394866c5c807e0a2
Henri Buyse <henri.buyse@gmail.com>
2014-06-04 22:17:50 +0200

    Déclaration de l'objet Tournoi

---
+++ src/com/flambaz/accrodestournois/Tournoi.java
@@ -0,0 +1,34 @@
+package com.flambaz.accrodestournois;
+
+public class Tournoi {
+	private String lieu;
+    private String details;
+    private int    jour;
+    private int    mois;
+ 
+    public Tournoi(String lieu, String details, int jour, int mois) {
+        super();
+        this.lieu = lieu;
+        this.details = details;
+        this.jour = jour;
+        this.mois = mois;
+    }
+    // getters and setters...
+    
+    public int getJour() {
+    	return this.jour;
+    }
+    
+    public int getMois() {
+    	return this.mois;
+    }
+    
+    public String getLieu() {
+    	return this.lieu;
+    }
+    
+    
+    public String getDetails() {
+    	return this.details;
+    }
+}
