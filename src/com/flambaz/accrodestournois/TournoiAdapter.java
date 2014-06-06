c4670e4d939fbda42c3aa3fae181ae79873b225e
Henri Buyse <henri.buyse@gmail.com>
2014-06-06 19:03:34 +0200

    Refonte de l'adapter des Tournois
    Il érite toujours de ArrayAdapter<Tournoi>

--- src/com/flambaz/accrodestournois/TournoiAdapter.java
+++ src/com/flambaz/accrodestournois/TournoiAdapter.java
@@ -11,39 +11,80 @@ import android.widget.TextView;
 
 public class TournoiAdapter extends ArrayAdapter<Tournoi> {
 	 
-        private final Context context;
+        @SuppressWarnings("unused")
+		private final Context context;
+        
+        // Déclaration de mon ArrayList de Tournoi
         private final ArrayList<Tournoi> tournoiArrayList;
 	 
-	        public TournoiAdapter(Context context, ArrayList<Tournoi> tournoiArrayList) {
+        /* here we must override the constructor for ArrayAdapter
+    	 * the only variable we care about now is ArrayList<Item> objects,
+    	 * because it is the list of objects we want to display.
+    	 */
+        public TournoiAdapter(Context context, int textViewResourceId, ArrayList<Tournoi> tournoiArrayList) {
+ 
+            super(context, textViewResourceId, tournoiArrayList);
+ 
+            this.context = context;
+            this.tournoiArrayList = tournoiArrayList;
+        }
 	 
-	            super(context, R.layout.adapter, tournoiArrayList);
-	 
-	            this.context = context;
-	            this.tournoiArrayList = tournoiArrayList;
-	        }
-	 
-	        @Override
-	        public View getView(int position, View convertView, ViewGroup parent) {
-	 
-	            // 1. Create inflater 
-	            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	 
-	            // 2. Get rowView from inflater
-	            View rowView = inflater.inflate(R.layout.adapter, parent, false);
-	 
-	            // 3. Get the two text view from the rowView
+    	/*
+    	 * we are overriding the getView method here - this is what defines how each
+    	 * list item will look.
+    	 */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+ 
+        	/* Assign the view we are converting to a local variable
+        	 */
+    		View rowView = convertView;
+
+    		/* First, check to see if the view is null. 
+    		 * If so, we have to inflate it.
+    		 * To "inflate it" basically means to render, or show, the view.
+    		 */ 
+    		if (rowView == null) {
+    			LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    			rowView = inflater.inflate(R.layout.adapter, null);
+    		}
+    		
+    		/* Recall that the variable position is sent in as an argument to this method.
+    		 * The variable simply refers to the position of the current object in the list.
+    		 * (The ArrayAdapter iterates through the list we sent it)
+    		 * 
+    		 * Therefore, i refers to the current Tournoi object.
+    		 */
+    		Tournoi i = tournoiArrayList.get(position);
+    		
+    		
+    		if (i != null) {
+    			/* This is how you obtain a reference to the TextViews.
+    			 * These TextViews are created in the XML files we defined.
+    			 */
 	            TextView lieuView    = (TextView) rowView.findViewById(R.id.firstLine);
 	            TextView detailsView = (TextView) rowView.findViewById(R.id.secondLine);
-	            TextView dateView    = (TextView) rowView.findViewById(R.id.date);
+	            TextView jourView    = (TextView) rowView.findViewById(R.id.date);
 	            TextView moisView    = (TextView) rowView.findViewById(R.id.mois);
-	 
-	            // 4. Set the text for textView 
-	            lieuView.setText(tournoiArrayList.get(position).getLieu());
-	            detailsView.setText(tournoiArrayList.get(position).getDetails());
-	            dateView.setText(tournoiArrayList.get(position).getJour());
-	            moisView.setText(tournoiArrayList.get(position).getMois());
-	            
-	            // 5. return rowView
-	            return rowView;
-	        }
+ 
+	            /* check to see if each individual textview is null.
+				 * if not, assign some text!
+				 */
+				if (lieuView != null){
+					lieuView.setText(i.getLieu());
+				}
+				if (detailsView != null){
+					detailsView.setText(i.getDetails());
+				}
+				if (detailsView != null){
+					moisView.setText(i.getMois());
+				}
+				if (jourView != null){
+					jourView.setText(i.getJour());
+				}
+    		}
+
+    		// the view must be returned to our activity
+            return rowView;
+        }
 }
