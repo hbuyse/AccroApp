7322e45bbea7b599c0f4697010a53e15435c27ce
Henri Buyse <henri.buyse@gmail.com>
2014-07-04 00:04:27 +0200

    Renaming, no more back button in list
     * Rename MainActivity to SplashScreen
     * Rename TournamentList to MainActivity
     * No more back button that was killing the app in the list of tournament

--- app/src/main/java/com/flambaz/accrodestournois/MainActivity.java
+++ app/src/main/java/com/flambaz/accrodestournois/MainActivity.java
@@ -1,47 +1,49 @@
 package com.flambaz.accrodestournois;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends Activity {
 
-    /* Splash screen timer
+    /* Called when the activity is first created.
      */
-    private static int SPLASH_TIME_OUT = 3000;
+    public void onCreate(Bundle savedInstanceState) {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tournament_list);
 
-        /* Remove the action bar from the activity
+        /* Create the list fragment and add it as our sole content.
          */
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
-
-        new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                    /* This method will be executed once the timer is over
-                     * Start your app main activity
-                     */
-                Intent i = new Intent(MainActivity.this, TournamentList.class);
-                startActivity(i);
-
-                    /* Close this activity
-                     */
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        if (getFragmentManager().findFragmentById(R.id.containerMain) == null) {
+            TournamentListFragment list = new TournamentListFragment();
+            getFragmentManager().beginTransaction().add(R.id.containerMain, list).commit();
+        }
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        /* Inflate the menu; this adds items to the action bar if it is present.
+         */
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /* Handle action bar item clicks here. The action bar will
+         * automatically handle clicks on the Home/Up button, so long
+         * as you specify a parent activity in AndroidManifest.xml.
+         */
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
